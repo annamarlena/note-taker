2de88ef7752498d4baecf4059da9daea42005790
Anna Marlena <annamarlena310@gmail.com>
2023-04-30 15:34:09 -0500

    Added files and started writing some code

--- api/server.js
+++ api/server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const path = require("path");
 const api = require("./api");
+const fs = require("fs");
+
+// application state
+let db = require("./db.json");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Insert code here for our static files and assets
-app.use(express.static('public'));
-
-// Insert code here so we can receive data 
+// our static files and assets
+app.use(express.static('client'));
+// receive data 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// app.post("/api <insert route to wildcard :id here>")   <---- may be needed later ----------
 
 app.use("/api", api);
 
 // Create a homepage route that points to homepage.html
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html')));
+  res.sendFile(path.join(__dirname, 'client/index.html'))
+);
 
 
 app.listen(PORT, () =>
